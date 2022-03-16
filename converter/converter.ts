b4367619980179e9817b31fb5eb6b9e72d8739ae
Fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2022-03-16 17:23:49 -0400

    Add Validation

--- converter/converter.ts
+++ converter/converter.ts
@@ -1,8 +1,15 @@
 import {parse, stringify} from 'yaml'
 import {ChatChatFormatsConfig} from "./types/chatchat";
+import Ajv from "ajv"
+
+const schema = require('./types/deluxechat.json');
+const ajv = new Ajv()
 
 export default function ConvertConfig(yamlconfig: string): string | false {
     const deluxechatConfig = parse(yamlconfig);
+    const validate = ajv.compile(schema)
+    const valid = validate(deluxechatConfig)
+    if (!valid) console.log(validate.errors)
     const chatchatFormatsConfig: ChatChatFormatsConfig = {
         "default-format": 'default',
         formats: {}
--- converter/types/deluxechat.ts
+++ converter/types/deluxechat.ts
@@ -1,8 +1,55 @@
 export interface DeluxeChatConfig {
     check_updates?: boolean;
     bungeecord?: {
-        enabled: boolean;
-        server_name: string;
-        server_prefix: string;
+        enabled?: boolean;
+        server_name?: string;
+        server_prefix?: string;
+        join_global?: boolean;
+        use_server_whitelist?: boolean;
+        server_whitelist?: string[]
+    }
+    relation_placeholders_enabled?: boolean;
+    timestamp_format?: string;
+    boolean?: {
+        true?: string;
+        false?: string;
+    }
+    ops_use_group_format?: boolean;
+    chat_filter?: {
+        enabled?: boolean;
+        ignore_case?: boolean;
+        list?: string[];
+    }
+    private_message?: {
+        enabled?: boolean;
+        bungeecord?: boolean;
+    }
+    private_message_formats?: Record<'to_sender' | 'to_recipient', {
+            format?: string,
+            tooltip?: string[],
+            click_command?: string
+            chat_color?: string
+        }
+    > & {
+        social_spy?: string
+    }
+    formats?: {
+        [key: string]: {
+            priority?: number;
+            channel: string;
+            prefix: string;
+            name_color: string;
+            name: string;
+            suffix: string;
+            chat_color: string;
+            channel_tooltip?: string[];
+            prefix_tooltip?: string[];
+            name_tooltip?: string[];
+            suffix_tooltip?: string[];
+            channel_click_command?: string;
+            prefix_click_command?: string;
+            name_click_command?: string;
+            suffix_click_command?: string;
+        }
     }
 }
