b00e9a7ab0b899e6a6265f4b8f54c9996165717b
Fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2022-03-21 16:29:04 -0400

    Update CLI

--- cli/generateschema.ts
+++ cli/generateschema.ts
@@ -3,10 +3,10 @@ export {}
 const tsj = require('ts-json-schema-generator');
 const fs = require('fs');
 
-const directory = fs.readdirSync('./converter/types/').filter((f: string) => f.endsWith('.ts'));
+const directory = fs.readdirSync('./converters/types/').filter((f: string) => f.endsWith('.ts'));
 
 directory.forEach((f: string) => {
-    const file = fs.readFileSync(`./converter/types/${f}`, 'utf8');
+    const file = fs.readFileSync(`./converters/types/${f}`, 'utf8');
 
     const regex = /\/\/ @ToolBox - ([a-zA-Z]{1,30})/;
     const match = file.match(regex);
@@ -14,11 +14,11 @@ directory.forEach((f: string) => {
     if (!match || match.length < 2) return;
 
     const schema = tsj.createGenerator({
-        path: `./converter/types/${f}`,
+        path: `./converters/types/${f}`,
         tsconfig: './tsconfig.json',
         type: match[1]
     }).createSchema(match[1])
 
     const schemaString = JSON.stringify(schema, null, 2);
-    fs.writeFileSync(`./converter/types/${f.replace('.ts', '.json')}`, schemaString);
+    fs.writeFileSync(`./converters/types/${f.replace('.ts', '.json')}`, schemaString);
 })
