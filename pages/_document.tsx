e7a5d939cc422c51c1feb4135dd4293a63be0447
Fredthedoggy <45927799+fredthedoggy@users.noreply.github.com>
2022-03-27 17:44:31 -0400

    I give up, copy/paste

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,8 +17,11 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="icon" href="/favicon.ico" />
-          <meta name="theme-color" content="#69137e" />
-          <meta property="og:image" content="logo.png" />
+          <meta
+            property="og:image"
+            content="https://helpch.at/includes/img/HC.png"
+          />
+          <meta name="theme-color" content="#40a1d5" />
         </Head>
         <body>
           <Main />
